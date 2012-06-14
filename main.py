afb2811a77a46224393745c075fe6cf6263f0f26
Ben Fields <me@benfields.net>
2012-06-14 18:38:02 +0200

    now returns json when asked

--- main.py
+++ main.py
@@ -36,12 +36,19 @@ def topN(request):
                peers=val) for rank, val, album in albums])
     return Response(doc_body)
     
+def topNjson(request):
+    albums = fetch_top_N_albums(int(request.matchdict.get('topN', 10)))
+    return Response(simplejson.dumps({'result':albums}))
+
 if __name__ == '__main__':
     config = Configurator()
     config.add_route('index', '')
+    config.add_route('topjson', '/top/{topN}.json')
     config.add_route('top', '/top/{topN}')
+    
     config.add_view(topN, route_name='top')
     config.add_view(topN, route_name='index')
+    config.add_view(topNjson, route_name='topjson')
     port = int(os.environ.get('PORT', 5000))
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
